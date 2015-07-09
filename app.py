9ca323fcf6c651f7c4da5943cddf3be5691c0e69
Rohan Mathur <rohanmathur34@gmail.com>
2015-07-08 23:12:34 -0400

    Add custom filtering and change up api spec a little
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

--- app.py
+++ app.py
@@ -24,18 +24,49 @@ items = [
     }
 ]
 
+
+"""
+Return the json of a 404 error
+"""
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
+
+"""
+Return the json of a 400 error
+"""
 @app.errorhandler(400)
 def not_found(error):
     return make_response(jsonify({'error': 'Bad request'}), 400)
 
+
+"""
+Get either a list of items, a specific item by id, or all the items of a certain type
+"""
 @app.route('/api/v1.0/items', methods=['GET'])
 def get_items():
-    return jsonify({'items': items})
+    if 'id' in request.args:
+        item = [item for item in items if str(
+            item['id']) == request.args['id']]
+        if len(item) == 0:
+            abort(404)
+        return jsonify({'item': item[0]})
+    elif 'type' in request.args:
+        item = [item for item in items if item['type'] == request.args['type']]
+        if len(item) == 0:
+            abort(404)
+        elif len(item) == 1:
+            return jsonify({'item': item})
+        else:
+            return jsonify({'items': item})
+    else:
+        return jsonify({'items': items})
 
+
+"""
+Add a new item to the database
+"""
 @app.route('/api/v1.0/items', methods=['POST'])
 def create_item():
     if not request.json or not 'name' in request.json or not 'type' in request.json:
@@ -49,13 +80,10 @@ def create_item():
     items.append(item)
     return jsonify({'item': item}), 201
 
-@app.route('/api/v1.0/items/<int:item_id>', methods=['GET'])
-def get_item(item_id):
-    item = [item for item in items if item['id'] == item_id]
-    if len(item) == 0:
-        abort(404)
-    return jsonify({'item': item[0]})
 
+"""
+Update an item by id
+"""
 @app.route('/api/v1.0/items/<int:item_id>', methods=['PUT'])
 def update_item(item_id):
     item = [item for item in items if item['id'] == item_id]
@@ -73,6 +101,10 @@ def update_item(item_id):
     item[0]['checkedIn'] = request.json.get('checkedIn', item[0]['checkedIn'])
     return jsonify({'item': item[0]})
 
+
+"""
+Delete an item by id
+"""
 @app.route('/api/v1.0/items/<int:item_id>', methods=['DELETE'])
 def delete_item(item_id):
     item = [item for item in items if item['id'] == item_id]
